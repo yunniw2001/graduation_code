9b8b2f48871f467f6a06c7cac05d02e4dd5c6dcb
yunniw2001 <2025151726@qq.com>
2023-02-04 15:18:52 +0800

    2.4: 子空间初步测试完毕

--- subspace_test.py
+++ subspace_test.py
@@ -46,87 +46,83 @@ def prepare_transform_for_image():
          transforms.ToTensor(),
          transforms.Normalize(0.5, 0.5)])
 
+def calculate_cos_similar(matrix,vector):
+    num = np.dot(vector.T,matrix)  # 向量点乘
+    denom = np.linalg.norm(matrix) * np.linalg.norm(vector)  # 求模长的乘积
+    print(num.shape)
+    res = num / denom
+    # print(res.shape)
+    # res[np.isneginf(res)] = 0
+    res = 0.5+0.5*res
+    return res[0]
 
-# 读入图像
-img_PATH = '/home/ubuntu/dataset/CASIA/test_session/session1/'
-label_PATH = '/home/ubuntu/dataset/CASIA/test_session/session1_label.txt'
+
+# 读入图片
+img_PATH = '/home/ubuntu/dataset/CASIA/test_session/session2/'
+label_PATH = '/home/ubuntu/dataset/CASIA/test_session/session2_label.txt'
 save_pca_PATH = '/home/ubuntu/graduation_model/palmmatrix_test_session1.joblib'
 save_numpy_PATH = '/home/ubuntu/graduation_model/palmmatrix_test_session1_numpy.npy'
 prepare_transform_for_image()
 palms = []
 palmmatrix = []
 palmlabel = []
-already_processed = True
 
 label_file = open(label_PATH, 'r')
 content = label_file.readlines()
 class_size = 0
 min_size = 1000000
-if not already_processed:
-    print('===start read train dataset!===')
-    for line in content:
-        img_name, img_label = line.split(' ')[0], int(line.split(' ')[1])
-        tmp_image_path = img_PATH + img_name
-        if img_label > class_size:
-            class_size = img_label
-        if img_label < min_size:
-            min_size = img_label
-        cur = preprocessing(Image.open(tmp_image_path).convert('L'))
-        # print(cur.size())
-        cur = cur.permute(1, 2, 0).detach().numpy()
-        palms.append(cur)
-        palmmatrix.append(cur.flatten())
-        palmlabel.append(img_label)
-        # if i >= 20:
-        #     break
-        # break
-    palmmatrix = np.array(palmmatrix)
-    # PCA
-    print('===start PCA!===')
-    pca = PCA().fit(palmmatrix)
-    print('===PCA DONE!===')
-    np.save(save_numpy_PATH, palmmatrix)
-    dump(pca, save_pca_PATH)
-else:
-    print('===start load pca!===')
-    palmmatrix = np.load(save_numpy_PATH)
-    pca = load(save_pca_PATH)
-    print('===start read label file!===')
-    for line in content:
-        img_name, img_label = line.split(' ')[0], int(line.split(' ')[1])
-        if img_label > class_size:
-            class_size = img_label
-        if img_label < min_size:
-            min_size = img_label
-        palmlabel.append(img_label)
 
-# 预览图片
-# fig, axes = plt.subplots(4,5,sharex=True,sharey=True,figsize=(8,10))
-# for i in range(20):
-#     axes[i%4][i//4].imshow(palms[i], cmap="gray")
-# plt.show()
-
+print('===start load test image!===')
+for line in content:
+    img_name, img_label = line.split(' ')[0], int(line.split(' ')[1])
+    tmp_image_path = img_PATH + img_name
+    if img_label > class_size:
+        class_size = img_label
+    if img_label < min_size:
+        min_size = img_label
+    cur = preprocessing(Image.open(tmp_image_path).convert('L'))
+    # print(cur.size())
+    cur = cur.permute(1, 2, 0).detach().numpy()
+    palms.append(cur)
+    palmmatrix.append(cur.flatten())
+    palmlabel.append(img_label)
+palmmatrix = np.array(palmmatrix)
 print('===%d images Done! %d classes in total!===' % (len(content), class_size - min_size + 1))
-# print(palmmatrix)
-# print(palmmatrix.shape)
-
-# 预览特征脸
+# 导入pca
+print('===start load pca!===')
+palmmatrix = np.load(save_numpy_PATH)
+pca = load(save_pca_PATH)
+# 生成特征脸
 n_components = 50
 eigenpalms = pca.components_[:n_components]
 
-# fig, axes = plt.subplots(4, 4, sharex=True, sharey=True, figsize=(8, 10))
-# for i in range(16):
-#     axes[i % 4][i // 4].imshow(eigenpalms[i].reshape(224, 224), cmap="gray")
-# plt.show()
-
 # 权重
 weights = eigenpalms @ (palmmatrix - pca.mean_).T
+
 # 测试
 print('===start test!===')
-goal = 0
-query = palmmatrix[goal].reshape(1, -1)
-query_weight = eigenpalms @ (query - pca.mean_).T
-euclidean_distance = np.linalg.norm(weights - query_weight, axis=0)
-best_match = np.argmin(euclidean_distance)
-print("Best match %s with Euclidean distance %f" % (palmlabel[best_match], euclidean_distance[best_match]))
-print('the correct answer is %d' % palmlabel[goal])
+
+idx = 0
+cur_correct = 0
+total_correct = 0
+batch = 0
+while idx < len(palmmatrix):
+    query = palmmatrix[idx].reshape(1, -1)
+    query_weight = eigenpalms @ (query - pca.mean_).T
+    # cos_similarity = calculate_cos_similar(weights,query_weight)
+    cos_similarity = np.linalg.norm(weights - query_weight, axis=0)
+    best_match = np.argmin(cos_similarity)
+    # print(palmlabel[best_match])
+    # print(len(palmlabel))
+    # print(palmlabel[idx])
+    if palmlabel[best_match] == palmlabel[idx]:
+        cur_correct+=1
+        total_correct+=1
+    if (idx+1)%100 == 0:
+        print('batch %d: correct rate = %.2f'%(batch,cur_correct/100))
+        cur_correct=0
+        batch+=1
+    idx += 1
+    # break
+
+print('TOTAL CORRECT RATE: %.2f'%(total_correct/len(palmmatrix)))
